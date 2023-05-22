b0162efffa0eec8085cc412c80e428a5b6b39f95
Darryl <darryl.a.nicholson@gmail.com>
2023-05-21 21:36:35 -0700

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./*.{php,html,js}','./components/blocks/**/*.{php,html,css,js}'],
+  content: ['./*.{php,html,js}', './components/blocks/**/*.{php,html,css,js}'],
   theme: {
     extend: {},
   },
