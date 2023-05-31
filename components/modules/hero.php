1bc6352b0c3a41e8dfbdc1f072a354f6a795356c
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2023-05-30 18:47:00 -0700

    update hero module

--- components/modules/hero.php
+++ components/modules/hero.php
@@ -18,10 +18,10 @@ $has_full_width = ACF::getField('background_has_full_width', $data);
 
 <div class="module hero relative <?php echo $has_full_width ? 'ddi_container_full' : 'ddi_container'; ?>" <?php echo Util::getInlineBackgroundStyles($data); ?>>
     <?php if ($headline || $content || $button) : ?>
-        <div class="uk-container grid z-10 text-center relative text-white h-[100%] justify-center content-center">
+        <div class="module__content uk-container w-[350px] md:w-[450px] lg:w-[550px] grid z-10 text-center relative text-white h-[100%] justify-center content-center">
             <?php
             if ($headline) {
-                echo '<div class="module__heading pb-8">';
+                echo '<div class="module__heading -translate-x-[250px] pb-8">';
                 echo nl2br(Util::getHTML(
                     $headline,
                     'h1',
@@ -31,12 +31,19 @@ $has_full_width = ACF::getField('background_has_full_width', $data);
             }
             ?>
             <?php if ($content) {
-                echo '<div class="module__body  text-xl">';
+                echo '<div class="module__body -translate-x-[250px] text-xl">';
                 echo apply_filters('the_content', $content);
                 echo '</div>';
             } ?>
             <?php if ($button) {
-                echo Util::getButtonHTML($button);
+                echo '<div class="module__link -translate-x-[250px]">';
+                printf(
+                    '<a class="flex mx-auto justify-center w-[200px] bg-blue-900 p-4 text-center" href="%2$s" target="%3$s">%1$s</a>',
+                    $button['title'],
+                    $button['url'],
+                    $button['target']
+                );
+                echo '</div>';
             } ?>
         </div>
     <?php endif; ?>
